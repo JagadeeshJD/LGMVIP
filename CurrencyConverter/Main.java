cf339feb9df0f377ce8011c6b604befead5bc907
JagadeeshJD <20pa1a0539@vishnu.edu.in>
2023-03-31 16:21:27 +0530

    updated

---
+++ CurrencyConverter/Main.java
@@ -0,0 +1,25 @@
+package LGMVIP.CurrencyConverter;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+
+            CurrencyConverter currencyConverter = new CurrencyConverter();
+
+            currencyConverter.setTitle("Currency Converter App");
+            currencyConverter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            currencyConverter.setSize(500, 400);
+            currencyConverter.setLocationRelativeTo(null);
+            currencyConverter.setVisible(true);
+        }
+        catch(Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+    }
+}
