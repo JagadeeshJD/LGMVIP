dde00b152c5a0002c204a8a2975519e9c2fb0b8c
JagadeeshJD <20pa1a0539@vishnu.edu.in>
2023-03-31 18:30:59 +0530

    added scientific calci

---
+++ ScientificCalculator/Main.java
@@ -0,0 +1,19 @@
+package LGMVIP.ScientificCalculator;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+
+        scientificCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        scientificCalculator.setSize(400, 350);
+        scientificCalculator.setVisible(true);
+
+
+    }
+}
